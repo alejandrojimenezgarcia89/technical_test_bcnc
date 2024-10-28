d9f7726e59cd52e42e5b96f0ff73b99b35cf8e9e
Alejandro <alejandrojimenezgarcia89@gmail.com>
2024-10-28 19:27:55 +0100

    chore: add readme

--- domain/src/main/java/com/technicaltest/bcnc/entity/Price.java
+++ domain/src/main/java/com/technicaltest/bcnc/entity/Price.java
@@ -9,11 +9,11 @@ import java.time.OffsetDateTime;
 @Builder
 public class Price {
 
+    private long productId;
     private int brandId;
     private OffsetDateTime startDate;
     private OffsetDateTime endDate;
     private int priceList;
-    private long productId;
     private int priority;
     private double productPrice;
     private String currency;
